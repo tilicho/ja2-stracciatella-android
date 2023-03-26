f0ab509d64b1e967aeb7fbb0ad0bbb75e7cabb9f
tilicho <berserksmail@gmail.com>
2023-03-26 17:41:23 +0300

    Revert "fix crash for release android7eabi (possibly because of strict aliasing rule)"
    This reverts commit 93a19cb48801ee0953fac59bfc10525598d140d8.

--- src/externalized/strategic/SamSiteAirControlModel.cc
+++ src/externalized/strategic/SamSiteAirControlModel.cc
@@ -35,7 +35,7 @@ SamSiteAirControlModel* SamSiteAirControlModel::deserialize(const JsonValue& obj
 		}
 		for (auto& col : row)
 		{
-			controlTable[i++] = static_cast<int8_t>(col.GetInt64());
+			controlTable[i++] = static_cast<int8_t>(col.toInt());
 		}
 	}
 
